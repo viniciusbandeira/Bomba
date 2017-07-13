81d542dcb8041af70cd3f17946370f1dbf6170d0
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-07-13 18:18:15 -0300

    Criando activity de desarmamento da bomba

---
+++ app/src/main/java/bandeira/bomba/DefuseActivity.java
@@ -0,0 +1,13 @@
+package bandeira.bomba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DefuseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defuse);
+    }
+}
